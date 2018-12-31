0aa493fd5659269bff24283b0f07670658bb8ea9
Ankur Kumar <richnusgeeks@gmail.com>
2018-12-30 23:05:02 -0800

    Add Apache Kafka to the stack.

--- AutoRemediation/scripts/setup.sh
+++ AutoRemediation/scripts/setup.sh
@@ -13,6 +13,11 @@ MNTECDIR="${MNTDIR}/monit.d"
 OSVRSNFL='/etc/os-release'
 MNTCRTFL='monit.pem'
 MNTCRTDIR='/etc/ssl/certs/'
+KFKVER='2.11'
+KFKDIR='/opt/kafka'
+KFKSVER='2.1.0'
+JREVERU='openjdk-8-jre-headless'
+JREVERR='java-1.8.0-openjdk-headless'
 
 exitOnErr() {
   local date=$($DATE)
@@ -20,50 +25,99 @@ exitOnErr() {
   exit 1
 }
 
-if ! command curl -V > /dev/null 2>&1
-then
-  apt-get update -y
-  apt-get install -y curl
-fi
+setupPreReq() {
 
-if ! curl -sSLk "https://mmonit.com/monit/dist/binary/${MNTVER}/monit-${MNTVER}-linux-x64.tar.gz" \
-          -o monit.tar.gz
-then
-  exitOnErr "monit ${MNTVER} tarball download failed"
-fi
+  if ! command -v curl > /dev/null 2>&1
+  then
+    apt-get update -y
+    apt-get install -y curl "${JREVERU}"
+  else
+    yum install -y "${JREVERR}"
+  fi
 
-tar zxvf monit.tar.gz
-mv monit-* "${MNTDIR}"
-mkdir "${MNTECDIR}"
-mv "${MNTCDIR}/${MNTCNFG}" "${MNTCDIR}/${MNTCNFG}.orig"
-rm -f monit.tar.gz
+}
 
-cat <<EOF | tee "${MNTCDIR}/${MNTCNFG}"
-set daemon  10
-set log /var/log/monit.log
+setupMonit() {
 
-#set mmonit http://monit:monit@MMONITIP:MMONITPORT/collector with timeout 10 seconds
+  if ! curl -sSLk "https://mmonit.com/monit/dist/binary/${MNTVER}/monit-${MNTVER}-linux-x64.tar.gz" \
+            -o monit.tar.gz
+  then
+    exitOnErr "monit ${MNTVER} tarball download failed"
+  fi
 
-set httpd port 2812 and
-  with ssl {
-    pemfile: /etc/ssl/certs/monit.pem
-  }
-  use address 0.0.0.0
-  allow localhost
-  allow 0.0.0.0/0
-  allow admin:monit
-  allow guest:guest readonly
+  tar zxf monit.tar.gz
+  mv monit-* "${MNTDIR}"
+  mkdir "${MNTECDIR}"
+  mv "${MNTCDIR}/${MNTCNFG}" "${MNTCDIR}/${MNTCNFG}.orig"
+  rm -f monit.tar.gz
 
-include "${MNTECDIR}/*"
+  cat <<EOF | tee "${MNTCDIR}/${MNTCNFG}"
+  set daemon  10
+  set log /var/log/monit.log
+
+  #set mmonit http://monit:monit@MMONITIP:MMONITPORT/collector with timeout 10 seconds
+
+  set httpd port 2812 and
+    with ssl {
+      pemfile: /etc/ssl/certs/monit.pem
+    }
+    use address 0.0.0.0
+    allow localhost
+    allow 0.0.0.0/0
+    allow admin:monit
+    allow guest:guest readonly
+
+  include "${MNTECDIR}/*"
 EOF
-chmod 0600 "${MNTCDIR}/${MNTCNFG}" "${MNTCRTDIR}/${MNTCRTFL}"
+  chmod 0600 "${MNTCDIR}/${MNTCNFG}" "${MNTCRTDIR}/${MNTCRTFL}"
 
-if ! curl -sSLk "https://mmonit.com/dist/mmonit-${MMNTVER}-linux-x64.tar.gz" \
-          -o mmonit.tar.gz
-then
-  exitOnErr "mmonit ${MMNTVER} tarball download failed"
-fi
+}
 
-tar zxvf mmonit.tar.gz 
-mv mmonit-* "${MMNTDIR}"
-rm -f mmonit.tar.gz
+setupMMonit() {
+
+  if ! curl -sSLk "https://mmonit.com/dist/mmonit-${MMNTVER}-linux-x64.tar.gz" \
+            -o mmonit.tar.gz
+  then
+    exitOnErr "mmonit ${MMNTVER} tarball download failed"
+  fi
+
+  tar zxf mmonit.tar.gz
+  mv mmonit-* "${MMNTDIR}"
+  rm -f mmonit.tar.gz
+
+}
+
+setupKafka() {
+
+  if ! curl -sSLk "https://www-eu.apache.org/dist/kafka/${KFKSVER}/kafka_${KFKVER}-${KFKSVER}.tgz" -o kafka.tgz
+  then
+    exitOnErr "kafka ${KFKVER}-${KFKSVER} tarball download failed"
+  fi
+
+  tar zxf kafka.tgz
+  mv kafka_* "${KFKDIR}"
+  rm -f kafka.tgz
+
+  cat << EOF | tee "${MNTECDIR}/zkprkfk"
+  check process zookeeper matching zookeeper
+    start program = "${KFKDIR}/bin/zookeeper-server-start.sh -daemon ${KFKDIR}/config/zookeeper.properties" with timeout 60 seconds
+    if failed port 2181 for 6 cycles then restart
+
+  check process kafka matching kafkaServer
+    depends on zookeeper
+    start program = "${KFKDIR}/bin/kafka-server-start.sh -daemon ${KFKDIR}/config/server.properties" with timeout 60 seconds
+    if failed port 9092 for 6 cycles then restart
+EOF
+
+}
+
+main() {
+
+  setupPreReq
+  setupMonit
+  setupMMonit
+  setupKafka
+
+}
+
+main 2>&1

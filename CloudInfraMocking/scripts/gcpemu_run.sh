f84941600acef319da1db15a48b4573f2fd67a2f
Ankur Kumar <richnusgeeks@gmail.com>
2019-02-02 00:36:58 -0800

    Dockerized setup of GCP emulators.

--- CloudInfraMocking/scripts/gcpemu_run.sh
+++ CloudInfraMocking/scripts/gcpemu_run.sh
@@ -5,4 +5,5 @@ gcloud beta emulators pubsub start > /var/log/pubsub.log 2>&1 &
 gcloud beta emulators datastore start > /var/log/datastore.log 2>&1 &
 gcloud beta emulators firestore start > /var/log/firestore.log 2>&1 &
 
-while true; do sleep 5; done
+sleep 5
+tail -f /var/log/*.log

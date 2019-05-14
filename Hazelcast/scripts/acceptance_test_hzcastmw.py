930eccf1555017356e74626933b43e66f0b33a79
RichNusGeeks <richnusgeeks@gmail.com>
2019-05-14 00:46:53 -0700

    Add group name/password args in the acceptance test.

--- Hazelcast/scripts/acceptance_test_hzcastmw.py
+++ Hazelcast/scripts/acceptance_test_hzcastmw.py
@@ -5,29 +5,34 @@ from time import sleep
 import random
 import hazelcast
 
-if len(argv) != 3:
-    print(" Usage: %s <ConnectionString e.g. hzhost1:port,hzhost2:port,...> <NumOfKeys e.g. 100000>" %argv[0])
+if len(argv) != 5:
+    print(" Usage: %s <ConnectionString e.g. hzhost1:port,hzhost2:port,...> <GroupName> <GroupPswrd> <NumOfKeys e.g. 100000>" %argv[0])
     exit(1)
 
 connstr = argv[1]
-numkeys = int(argv[2])
+grpname = argv[2]
+grppswd = argv[3]
+numkeys = int(argv[4])
 
 config = hazelcast.ClientConfig()
+config.group_config.name = grpname
+config.group_config.password = grppswd
 for hp in connstr.split(','):
   config.network_config.addresses.append(hp)
 client = hazelcast.HazelcastClient(config)
 
-m = client.get_map("smoketest")
-for k in xrange(1, numkeys+1):
-  m.put("%s" %k, random.randint(1,numkeys))
+if numkeys > 0:
+  m = client.get_map("smoketest")
+  for k in xrange(1, numkeys+1):
+    m.put("%s" %k, random.randint(1,numkeys))
 
-for c in xrange(10):
-  lock = client.get_lock("smoketestlock").blocking()
-  lock.lock()
-  try:
-    print(" locking %d time" %(int(c)+1))
-    sleep(1)
-  finally:
-    lock.unlock()
+  for c in xrange(10):
+    lock = client.get_lock("smoketestlock").blocking()
+    lock.lock()
+    try:
+      print(" locking %d time" %(int(c)+1))
+      sleep(1)
+    finally:
+      lock.unlock()
 
 client.shutdown()

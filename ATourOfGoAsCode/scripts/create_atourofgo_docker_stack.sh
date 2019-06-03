98eaee8906193005d010a92eb11c2aeb8d961a5f
Ankur Kumar <richnusgeeks@gmail.com>
2019-06-02 23:51:43 -0700

    Launch A Tour of Go locally through docker.

---
+++ ATourOfGoAsCode/scripts/create_atourofgo_docker_stack.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+
+OPTN=${1}
+NUMOPTNMX=2
+CMPSFLDIR='.'
+CMPSEFILE='atourofgo_stack.yml'
+RQRDCMNDS="docker
+           docker-compose"
+
+preReq() {
+
+  for c in ${RQRDCMNDS}
+  do
+    if ! command -v "${c}" > /dev/null 2>&1
+    then
+      echo " Error: required command ${c} not found, exiting ..."
+      exit -1
+    fi
+  done
+
+}
+
+printUsage() {
+
+  echo " Usage: $(basename $0) < up|buildup|ps|logs|down|cleandown >"
+  exit 0
+
+}
+
+parseArgs() {
+
+  if [[ $# -gt ${NUMOPTNMX} ]]
+  then
+    printUsage
+  fi
+
+  if [[ "${OPTN}" != "up" ]] && \
+     [[ "${OPTN}" != "ps" ]] && \
+     [[ "${OPTN}" != "logs" ]] && \
+     [[ "${OPTN}" != "down" ]] && \
+     [[ "${OPTN}" != "cleandown" ]] && \
+     [[ "${OPTN}" != "buildup" ]]
+  then
+    printUsage
+  fi
+
+}
+
+main() {
+
+  parseArgs
+
+  preReq
+
+  if [[ "${OPTN}" = "up" ]]
+  then
+    docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" "${OPTN}" -d
+  elif [[ "${OPTN}" = "buildup" ]]
+  then
+    docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" up --build -d
+  elif [[ "${OPTN}" = "cleandown" ]]
+  then
+    docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" down -v
+  else
+    docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" "${OPTN}"
+  fi
+
+}
+
+main 2>&1

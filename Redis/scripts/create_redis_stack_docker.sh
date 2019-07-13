0b12c553e16df083bdc2aa310f0a3a1e711fbe5e
Ankur Kumar <richnusgeeks@gmail.com>
2019-07-12 20:00:14 -0700

    Dockerize master/slave/cli redis setup.

--- Redis/scripts/create_redis_stack_docker.sh
+++ Redis/scripts/create_redis_stack_docker.sh
@@ -1,31 +1,65 @@
 #! /bin/bash
 
 OPTN=${1}
-NUMOPTNMX=2
+SRVC=${2}
+SHELL=${3}
+NUMOPTNMX=4
 CMPSFLDIR='.'
 CMPSEFILE='redis_stack.yml'
+RQRDCMNDS="cat
+           docker
+           docker-compose
+           echo"
+
+preReq() {
+
+  for c in ${RQRDCMNDS}
+  do
+    if ! command -v "${c}" > /dev/null 2>&1
+    then
+      echo " Error: required command ${c} not found, exiting ..."
+      exit -1
+    fi
+  done
+
+}
 
 printUsage() {
 
-  echo " Usage: $(basename $0) < up|buildup|ps|logs|down|cleandown >"
+  cat <<EOF
+  Usage: $(basename $0) < up|buildup|ps|exec <name> <cmnd>
+                            |logs|down|cleandown >
+EOF
   exit 0
 
 }
 
-if [[ $# -gt ${NUMOPTNMX} ]]
-then
-  printUsage
-fi
+parseArgs() {
 
-if [[ "${OPTN}" != "up" ]] && \
-   [[ "${OPTN}" != "ps" ]] && \
-   [[ "${OPTN}" != "logs" ]] && \
-   [[ "${OPTN}" != "down" ]] && \
-   [[ "${OPTN}" != "cleandown" ]] && \
-   [[ "${OPTN}" != "buildup" ]]
-then
-  printUsage
-else
+  if [[ $# -gt ${NUMOPTNMX} ]]
+  then
+    printUsage
+  fi
+
+  if [[ "${OPTN}" != "up" ]] && \
+     [[ "${OPTN}" != "ps" ]] && \
+     [[ "${OPTN}" != "logs" ]] && \
+     [[ "${OPTN}" != "down" ]] && \
+     [[ "${OPTN}" != "cleandown" ]] && \
+     [[ "${OPTN}" != "buildup" ]] &&
+     [[ "${OPTN}" != "exec" ]]
+  then
+    printUsage
+  fi
+
+}
+
+main() {
+
+  parseArgs
+
+  preReq
+
   if [[ "${OPTN}" = "up" ]]
   then
     docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" "${OPTN}" -d
@@ -35,7 +69,13 @@ else
   elif [[ "${OPTN}" = "cleandown" ]]
   then
     docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" down -v
+  elif [[ "${OPTN}" = "exec" ]]
+  then
+    exec docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" exec "${SRVC}" "${SHELL}"
   else
     docker-compose -f "${CMPSFLDIR}/${CMPSEFILE}" "${OPTN}"
   fi
-fi
+
+}
+
+main 2>&1
